19982679f20b9a52881bf4f89eec142eae55c53e
higorcos <higorcosta2001@outlook.com>
2021-07-17 23:02:11 -0300

    update

--- src/router.js
+++ src/router.js
@@ -1,29 +1,41 @@
 const express = require('express')
 const routes = express.Router();
-
-const Users = require('../src/model/Users')
-
+const adminAuth = require('../src/middlerware/adminAuth');
 const FormController = require('../src/controllers/formController')
 
-routes.get('/',(req,res)=>{ res.redirect('/createdUser')});
-routes.get('/createdUser',FormController.createdUser);
-routes.post('/createdUser',FormController.saveUser);
+routes.get('/', (req, res) => { res.redirect('/createdUser') });
+routes.get('/createdUser', FormController.createdUser);
+routes.post('/createdUser', FormController.saveUser);
 routes.get('/login', FormController.login);
-routes.post('/login',FormController.loggingIn);
+routes.post('/login', FormController.loggingIn);
 
-routes.get('/event/:idUser?',FormController.show_event_open); //Mostra eventos em aberto
+routes.get('/event/:idUser?', FormController.show_event_open); //Mostra eventos em aberto
 
-routes.post('/event/sub/:idEvent',FormController.subscriptionEvent); //inscrição em evento
+routes.post('/event/sub/:idEvent', FormController.subscriptionEvent); //inscrição em evento
 
-routes.post('/user/redirect',FormController.redirectMenu); //Redireciona para login ou menu
-routes.get('/user/event/:idUser',FormController.showEvent);  // lista os evento inscrito
+routes.post('/user/redirect', FormController.redirectMenu); //Redireciona para login ou menu
+routes.get('/user/event/:idUser', adminAuth, FormController.showEvent);  // lista os evento inscrito
 
-routes.get('/admin/event/new',FormController.newEvent); //criar evento
-routes.post('/admin/event/save',FormController.saveEvent);
-//criar painel do adm 
+routes.get('/admin/event/new', FormController.newEvent); //criar evento
+routes.post('/admin/event/save', FormController.saveEvent);
 
-routes.get('/user/event/print/:idEvent',FormController.printCertificate) //Página html com certificado 
-routes.get('/user/event/print/emit/:idEvent',FormController.emitCertificate) //emissão do pdf (certificado)
+routes.get('/user/event/print/:idEvent', FormController.printCertificate) //Página html com certificado 
+routes.get('/user/event/print/emit/:idEvent', FormController.emitCertificate) //emissão do pdf (certificado)
 
+//painel do adm
+routes.get('/admin/permissions', FormController.permissionsShow) // Página para mostrar todas as permissões criadas
+routes.get('/admin/permissions/new', FormController.permissionsNew) // Página para criar permissões
+routes.post('/admin/permissions/save',FormController.permissionsSave); //rota post criação permissões 
 
-module.exports = routes 
+routes.get('/rec', (req, res) => {
+    req.session.prot = 23;
+    res.send("feito")    
+})
+routes.get('/white', (req, res) => {
+
+    res.json({
+        "sd": req.session.prot
+    })
+})
+
+module.exports = routes
--- src/server.js
+++ src/server.js
@@ -3,28 +3,33 @@ const routes = require('./router')
 const path = require('path')
 const database = require('../src/database/db')
 const server = express();
+const session = require('express-session')
 
 //usar o template engine 
 server.set('view engine', 'ejs')
 //localização da pasta view
-server.set('views',path.join(__dirname, 'views'))
+server.set('views', path.join(__dirname, 'views'))
 //habilita arquivos statics
 server.use(express.static('public'))
 //usar o req.body
-server.use(express.urlencoded({extended: true}))
-
+server.use(express.urlencoded({ extended: true }))
+//seções
+server.use(session({
+    secret: "a37f4b1dc00c407faf06349fe71b6fb2",  //texto aleatório para aumentar a segurança
+    cookie: { maxAge: 86400000 } //max age 1 dia 86400000
+}))
 //conexão com o banco de dados 
 database
-.authenticate()
-.then(() => {
-    console.log('<<<<<<<<<<Conectado ao Banco de dados')
-}).catch((err) => {
-    console.log(err)
-    console.log("Erro no banco de dados")
-}) 
+    .authenticate()
+    .then(() => {
+        console.log('<<<<<<<<<<Conectado ao Banco de dados')
+    }).catch((err) => {
+        console.log(err)
+        console.log("Erro no banco de dados")
+    })
 
 //rotas
 server.use(routes)
 
 const port = 8080
-server.listen(port, ()=> console.log(`Server Online, port ${port}`))
+server.listen(port, () => console.log(`Server Online, port ${port}`))
